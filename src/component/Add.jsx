72e090b0e205105570d07aeab305c6e8a8760a79
audrey <vaniaaudrey15@gmail.com>
2023-05-26 21:10:03 +0700

    dev

---
+++ src/component/Add.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addList } from "../redux/action/todoAction";
+
+const Add = () => {
+  const [inputValue, setInputValue] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addList(inputValue));
+    setInputValue("");
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <section id="add">
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="What to do"
+          value={inputValue}
+          onChange={handleChange}
+        />{" "}
+        <button type="submit"> Add </button>{" "}
+      </form>{" "}
+    </section>
+  );
+};
+
+export default Add;
---
+++ src/component/List.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import DoneIcon from "../assets/done-icon.svg";
+import EditIcon from "../assets/edit-icon.svg";
+import DeleteIcon from "../assets/delete-icon.svg";
+import PropTypes from "prop-types";
+import { doneList, editList, removeList } from "../redux/action/todoAction";
+import { useDispatch } from "react-redux";
+
+const List = (props) => {
+  const [isEditPopUp, setEditPopUp] = useState(false);
+  const [editInputValue, setEditInputValue] = useState("");
+
+  const toggleEdit = () => {
+    setEditPopUp(!isEditPopUp);
+    setEditInputValue("");
+  };
+
+  const dispatch = useDispatch();
+
+  const handleDone = (index) => {
+    dispatch(doneList(index));
+  };
+
+  const handleRemove = (index) => {
+    dispatch(removeList(index));
+  };
+
+  const handleEdit = (index, inputValue) => {
+    dispatch(editList(index, inputValue));
+    toggleEdit();
+  };
+
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    handleEdit(props.urutan, editInputValue);
+  };
+
+  const handleChange = (e) => {
+    setEditInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="list-container">
+        <div className="left-list">
+          {" "}
+          {props.button && (
+            <img
+              src={DoneIcon}
+              alt=""
+              onClick={() => handleDone(props.urutan)}
+            />
+          )}{" "}
+          <p> {props.value} </p>{" "}
+        </div>{" "}
+        {props.button && (
+          <div className="right-list">
+            <img
+              src={EditIcon}
+              alt=""
+              onClick={() => toggleEdit(props.urutan)}
+            />{" "}
+            <img
+              src={DeleteIcon}
+              alt=""
+              onClick={() => handleRemove(props.urutan)}
+            />{" "}
+          </div>
+        )}
+      </div>
+      {isEditPopUp && (
+        <div className="edit-popup">
+          <form action="" onSubmit={handleSubmit}>
+            <label htmlFor=""> Edit To do </label>{" "}
+            <input
+              type="text"
+              placeholder={props.value}
+              value={editInputValue}
+              onChange={handleChange}
+            />{" "}
+            <div className="button">
+              <p onClick={() => toggleEdit(props.urutan)}> Cancel </p>{" "}
+              <button type="submit"> Edit </button>{" "}
+            </div>{" "}
+          </form>{" "}
+        </div>
+      )}
+      {isEditPopUp && <div className="overlay"> </div>}{" "}
+    </>
+  );
+};
+
+List.propTypes = {
+  button: PropTypes.bool.isRequired,
+  value: PropTypes.string.isRequired,
+  urutan: PropTypes.number.isRequired,
+};
+
+export default List;
